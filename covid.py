d4ad3836674a28997e4f31b4c8d4718871803111
kocagozhkn <30779741+kocagozhkn@users.noreply.github.com>
2021-04-25 16:26:38 +0300

    Create covid.py

---
+++ covid.py
@@ -0,0 +1,33 @@
+age = input("Are you a cigarette addict older than 75 years old? ")
+chronic = input("Do you have a severe chronic disease?")
+immune = input("Is your immune system too weak?")
+risk = False
+
+if age == "Yes":
+    age = True
+else:
+    age=False
+
+if chronic == "Yes":
+    chronic = True
+else:
+    chronic=False
+
+if immune == "Yes":
+    immune = True
+else:
+    immune = False
+
+
+
+if immune or age or chronic:
+    risk =True
+
+else:
+    risk = False
+
+if risk:
+    print("You are in risky group")
+
+else:
+    print("You are not in risky group")
